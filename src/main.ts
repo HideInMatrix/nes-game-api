35400b0f982b7ea526be31771b11c515810d2392
HideInMatrix <davidmorgancreate@gmail.com>
2025-02-04 21:36:08 +0800

    fix: 跨域问题修复

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  // 允许来自 http://localhost:3000 的请求
-
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://game.micromatrix.org',
     credentials: true, // 允许发送 Cookie
   });
   await app.listen(7000);
