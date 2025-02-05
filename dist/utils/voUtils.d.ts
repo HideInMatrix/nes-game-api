176c6c9ef66833e85c3e8578a64160cfce34de37
HideInMatrix <davidmorgancreate@gmail.com>
2025-02-05 14:03:58 +0800

    fix: 项目部署问题

--- dist/utils/voUtils.d.ts
+++
@@ -1,5 +0,0 @@
-import { ResultVO } from '@/vo/result.vo';
-export declare class VOUtils {
-    static success<T>(data?: T): ResultVO<T>;
-    static error(code: number, msg: string): ResultVO<null>;
-}
